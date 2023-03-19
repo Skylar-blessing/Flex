bb2a352e98bcf8cdd6bb5ccb59169f32f2e464e3
Skylar <skylarblessings5@gmail.com>
2023-03-20 02:16:46 +0300

    assignment completed

---
+++ file3.js
@@ -0,0 +1,22 @@
+function grossSalary() {
+    const grossSalary = "basic salary + benefits"
+    if (grossSalary <= 6,000){
+    console.log(tier = "I");
+    }else {
+        console.log(tier = "II")
+    }
+}
+
+let NSSF Deduction = "Pensionable Pay * 0.06*x";
+let x = "number";
+if (Tier I) {
+    NSSF Deduction = "Pensionable Pay * 0.06 * 2 (for both employer and employee) "
+} else {
+    NSSF Deduction = "Pensionable Pay * 0.06 * 2 * 12/52 (for both employer and employee) "
+}
+NHIF Deduction = 'Deduction Amount corresponding to the employee's Gross Pay';
+if gross_salary <= 5,999: nhif_deduction = 150 elif gross_salary <= 7,999: nhif_deduction = 300 elif gross_salary <= 11,999: nhif_deduction = 400 elif gross_salary <= 14,999: nhif_deduction = 500 elif gross_salary <= 19,999: nhif_deduction = 600 elif gross_salary <= 24,999: nhif_deduction = 750 elif gross_salary <= 29,999: nhif_deduction = 850 elif gross_salary <= 34,999: nhif_deduction = 900 elif gross_salary <= 39,999: nhif_deduction = 950 else: nhif_deduction = 1,000
+
+f total_taxable_income <= 288,000: paye_tax = total_taxable_income * 0.1 elif total_taxable_income <= 388,000: paye_tax = 24,000 * 0.1 + (total_taxable_income - 288,000) * 0.25 else: paye_tax = 24,000 * 0.1 + 8,333 * 0.25 + (total_taxable_income - 388,000) * 0.3
+
+net_salary = "gross_salary - nssf_deduction - nhif_deduction - paye_tax"
